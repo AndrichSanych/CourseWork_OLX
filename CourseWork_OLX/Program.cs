427c7e828fff45542775b88e0c9d72429edb3ba0
Sashok9203 <kovalchuk.olex@gmail.com>
2024-07-04 18:48:00 +0300

    Cors added

--- CourseWork_OLX/Program.cs
+++ CourseWork_OLX/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connStr = builder.Configuration.GetConnectionString("DefaultConnection")!;
 builder.Services.AddOlxDbContext(connStr);
 // Add services to the container.
+builder.Services.AddMainServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
