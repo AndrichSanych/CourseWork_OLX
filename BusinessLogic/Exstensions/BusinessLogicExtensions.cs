8cce87c36b4801207a86c8665def417e39c1f5be
Sashok9203 <kovalchuk.olex@gmail.com>
2024-07-05 15:41:33 +0300

    Image service added

---
+++ BusinessLogic/Exstensions/BusinessLogicExtensions.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogic.Interfaces;
+using BusinessLogic.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Exstensions
+{
+    public static class BusinessLogicExtensions
+    {
+        public static void AddBusinessLogicServices(this IServiceCollection services)
+        {
+            services.AddScoped<IImageService, ImageService>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+    }
+}
